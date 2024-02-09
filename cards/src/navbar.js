9f65a9b03a7c573feb23771e5fb26d8b613186d1
satvika <satvikafirangi@gmail.com>
2024-02-09 17:18:00 +0530

    all files are pushed

---
+++ cards/src/navbar.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './navbar.css';
+
+function navbar() {
+  return (
+    <div className="navbar">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <h1>it is a navbar</h1>
+      <button>navbar</button>
+    </div>
+  );
+}
+
+export default navbar;
